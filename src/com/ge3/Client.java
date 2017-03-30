3c982faa248303c2b409ff34d44da2aff2deab6f
tripleys <akseligamedev@gmail.com>
2017-03-30 14:57:52 +0100

    -

--- src/com/ge3/Client.java
+++ src/com/ge3/Client.java
@@ -1,16 +1,68 @@
 package com.ge3;
 
-import com.ge3.entity.Player;
+import java.awt.BorderLayout;
+import java.awt.Graphics;
+
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 
 /**
  * 
  * @author Akseli
  */
-public class Client {
+public class Client extends JComponent {
 
-	private Player myPlayer;
+	private static final long serialVersionUID = 1L;
 	
-	public Client() {
-		
+	private JFrame frame;
+	private JPanel panel;
+	
+	
+	protected Client() {
+		frame = new JFrame();
+		panel = new JPanel(new BorderLayout());
+		panel.add(this);
+		frame.add(panel);
+		frame.setSize(Constants.WIDTH, Constants.HEIGHT);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		processMainLoop();
+	}
+	
+	private void processMainLoop() {
+		new Thread() {
+			@Override
+			public void run() {
+				while(true) {
+					processGameLoop();
+					repaint();
+					try {
+						sleep(1);
+					} catch(Exception e) {
+						e.printStackTrace();
+						break;
+					}
+				}
+			}
+		}.start();
+	}
+	
+	private void processGameLoop() {
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+	}
+	
+	public JFrame getFrame() {
+		return frame;
+	}
+	
+	public JPanel getPanel() {
+		return panel;
 	}
 }
---
+++ src/com/ge3/Constants.java
@@ -0,0 +1,7 @@
+package com.ge3;
+
+public class Constants {
+
+	public static final int WIDTH = 700;
+	public static final int HEIGHT = 500;
+}
